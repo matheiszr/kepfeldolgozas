60abab6535f329cdbecb345f928d71a5baeb74ec
pataiadam <pataiadam@gmail.com>
2016-11-26 18:40:34 +0100

    Add imageViewer and ImagePathReader

---
+++ PokerCardRecognition/src/main/java/hu/szte/imageprocessing/cardrecognition/ImagePathReader.java
@@ -0,0 +1,92 @@
+package hu.szte.imageprocessing.cardrecognition;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+
+public class ImagePathReader{
+
+	/**
+	 * This method read all file location in the path and store these in a list.
+	 * @param directoryPath (a path to a directory which contains script files)
+	 * @param isDefault (use the default settings or not)
+	 * @return cqlFiles (path of script files)
+	 */
+	public String getOneImagePath(String file, boolean isDefault) throws Exception{
+		String result = "";
+		try {
+			BufferedReader br;
+			System.out.println("run: " + file);
+			if (isDefault){ // default way get the script file from the jar
+				br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/" + file)));
+			} else { // own setting way
+				br = new BufferedReader(new FileReader(file));
+			}
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();			
+			
+			while (line != null) {
+				sb.append(line);
+	            line = br.readLine();		            
+			}
+			br.close();
+			result = sb.toString();
+		} catch(Exception e) {
+			throw new Exception(e);
+		}
+		return result;
+	}
+
+	/**
+	 * This method read all file location in the path and store these in a list.
+	 * @param directoryPath (a path to a directory which contains script files)
+	 * @param isDefault (use the default settings or not)
+	 * @return cqlFiles (path of script files)
+	 */
+	public List<String> getAllImageFilePathFromADirectory(String directoryPath, boolean isDefault){
+		System.out.println("Start files reading...");
+		List<String> scriptFiles = new ArrayList<String>();
+		try {
+			if (isDefault){
+				File jarFile = new File(ImagePathReader.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+				if(jarFile.isFile()) {  // Run with JAR file
+					final JarFile jar = new JarFile(jarFile);
+					final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+					while(entries.hasMoreElements()){
+						final String name = entries.nextElement().getName();
+						StringBuilder sb = new StringBuilder("");
+						sb.append(directoryPath);
+						sb.append("\\");
+						if (name.startsWith(sb.toString())) { //filter according to the path
+							scriptFiles.add(name);
+						}
+					}
+					jar.close();
+				}
+			} else {
+				System.out.println(directoryPath);
+				File file = new File(directoryPath);
+				String files[] = file.list();
+				for (String f : files){
+					StringBuilder sb = new StringBuilder("");
+					sb.append(directoryPath);
+					sb.append("\\");
+					sb.append(f);
+					scriptFiles.add(sb.toString());
+				}
+			}
+		} catch(Exception e){
+			System.err.println("An error was occurred during the image reading.");
+			e.printStackTrace();
+		}
+		System.out.println("Image readings was finished successfully.");
+		return scriptFiles;
+	}
+}
--- PokerCardRecognition/src/main/java/hu/szte/imageprocessing/cardrecognition/Main.java
+++ PokerCardRecognition/src/main/java/hu/szte/imageprocessing/cardrecognition/Main.java
@@ -1,29 +1,43 @@
 package hu.szte.imageprocessing.cardrecognition;
 
+import hu.szte.imageprocessing.cardrecognition.entity.Card;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import hu.szte.imageprocessing.cardrecognition.entity.Card;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.KeyPoint;
+import org.opencv.highgui.Highgui;
 
 /**
- * This class is the main class of the program.
- * This contains the main method which make 
- * methods on input and training Images 
- * and manage the console program.
+ * This class is the main class of the program. This contains the main method
+ * which make methods on input and training Images and manage the console
+ * program.
+ * 
  * @author Zsarnok
  *
  */
-public class Main{
-	 
-	public static void main(String[] args){
-		try{
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
 			// read the test Image
 			int inputOption = 0;
-			while(inputOption <= 0 || inputOption >= 4){
-				while(inputOption <= 0 || inputOption >= 4){
+			learning();
+			while (inputOption <= 0 || inputOption >= 4) {
+				while (inputOption <= 0 || inputOption >= 4) {
 					System.out.println("Pleas choose the following options:");
 					System.out.println("1 : load a test Image fro");
 					System.out.println("2 : count default test Images");
@@ -31,55 +45,85 @@ public class Main{
 					System.out.println();
 					inputOption = Integer.parseInt(br.readLine());
 				}
-				if(inputOption == 1){
-					System.out.println("Pleas type the full path to the Image:");
+				if (inputOption == 1) {
+					System.out
+							.println("Pleas type the full path to the Image:");
 					System.out.println();
 					String inputPath = br.readLine();
 					runCountOnInputImage(inputPath);
 				}
-				if(inputOption == 2)
-					runDefaultTestImageCounts();	
-				if(inputOption == 3)
+				if (inputOption == 2)
+					runDefaultTestImageCounts();
+				if (inputOption == 3)
 					return;
 				System.out.println();
 			}
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-		
-	private static void runDefaultTestImageCounts() throws Exception{
-		List<String> ImagePaths = new ArrayList<String>(); // FIXME feltöltés
-		for(String s : ImagePaths){
+
+	private static void runDefaultTestImageCounts() throws Exception {
+		List<String> ImagePaths = new ArrayList<String>();
+		for (String s : ImagePaths) {
 			readAndStoreOneImage(s);
 			whatIsTheHand();
 		}
-	}	
-	
-	private static void runCountOnInputImage(String imagePath) throws Exception{
+	}
+
+	private static void learning() throws Exception {
+		List<String> ImagePaths = new ImagePathReader()
+				.getAllImageFilePathFromADirectory(
+						System.getProperty("user.dir")
+								+ "\\src\\main\\resources\\pictures\\learning",
+						false);
+		for (String s : ImagePaths) {
+			learnImage(s);
+		}
+	}
+
+	private static void learnImage(String s) {
+		// TODO Auto-generated method stub
+		Mat objectImage = Highgui.imread(s, Highgui.CV_LOAD_IMAGE_COLOR);
+        MatOfKeyPoint objectKeyPoints = new MatOfKeyPoint();
+        FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.SURF);
+        System.out.println("Detecting key points...");
+        featureDetector.detect(objectImage, objectKeyPoints);
+        KeyPoint[] keypoints = objectKeyPoints.toArray();
+        System.out.println(keypoints);
+
+        MatOfKeyPoint objectDescriptors = new MatOfKeyPoint();
+        DescriptorExtractor descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.SURF);
+        System.out.println("Computing descriptors...");
+        descriptorExtractor.compute(objectImage, objectKeyPoints, objectDescriptors);
+
+	}
+
+	private static void runCountOnInputImage(String imagePath) throws Exception {
 		readAndStoreOneImage(imagePath);
 		whatIsTheHand();
 	}
-	
-	private static void readAndStoreOneImage(String pathToImage){
-		//FIXME kell a beolvasás és a lementése a képnek
+
+	private static void readAndStoreOneImage(String pathToImage) {
+		// FIXME kell a beolvasás és a lementése a képnek
+		Highgui.imread(pathToImage);
 	}
-	
-	private static void whatIsTheHand() throws Exception{
+
+	private static void whatIsTheHand() throws Exception {
 		ImageToCardsConverter imageToCardsConverter = new ImageToCardsConverter();
 		List<Card> hand = imageToCardsConverter.getCardsFromImage();
-		
-		if(hand == null){
+
+		if (hand == null) {
 			System.out.println("Can't find 5 cards on the input image.");
 			return;
 		}
-		
+
 		HandCounter handCounter = new HandCounter(hand);
 		handCounter.countHand();
-		
+
 		System.out.println("Cards from image:");
 		StringBuilder sb = new StringBuilder("");
-		for(Card c : hand){
+		for (Card c : hand) {
 			sb.append("|");
 			sb.append(c.getSuit());
 			sb.append(" ");
@@ -88,6 +132,6 @@ public class Main{
 		}
 		System.out.println(sb.toString());
 	}
-	
-	//private keptarolo a beolvasott kepnek //FIXME ez még kell
+
+	// private keptarolo a beolvasott kepnek //FIXME ez még kell
 }
